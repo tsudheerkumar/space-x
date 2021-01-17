fde8823d4a9e3e546243de0c5009609356148b93
Tumarada <stumarada@dxc.com>
2021-01-17 11:45:16 +0530

    added server side rendering

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Hello Learn React
         </a>
       </header>
     </div>
